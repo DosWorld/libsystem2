8449e3e8afd2b7561391d4a45c322069071ed271
DosWorld <dosworld@github.com>
2025-04-01 16:56:47 +0300

    Change arrays

---
+++ dynarr.pas
@@ -0,0 +1,273 @@
+{ MIT License
+
+Copyright (c) 2022 Viacheslav Komenda
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. }
+{$A+,I-,S-,R-,D+,L+,Q-,F-,G-,O-,B-}
+UNIT DynArr;
+
+INTERFACE
+
+TYPE
+DYNARR_FILE = FILE;
+
+PArray = ^TArray;
+TArray = RECORD
+        Data        : POINTER;
+        Size        : WORD;
+        Capacity    : WORD;
+        ElementSize : WORD;
+END;
+
+PROCEDURE Init(VAR Arr : TArray; ElementSize : WORD);
+PROCEDURE Reset(VAR Arr : TArray);
+PROCEDURE Free(VAR Arr : TArray);
+
+PROCEDURE SetLength(VAR Arr : TArray; NewLength : WORD);
+FUNCTION GetLength(VAR Arr : TArray)  : WORD;
+PROCEDURE Delete(VAR Arr : TArray; StartIndex, Count: WORD);
+
+PROCEDURE Clear(VAR Arr : TArray; StartIndex, Count: WORD);
+PROCEDURE Insert(VAR Arr : TArray; Index: WORD; VAR Element);
+PROCEDURE Add(VAR Arr : TArray; VAR Element);
+PROCEDURE Get(VAR Arr : TArray; Index : WORD; VAR Element);
+FUNCTION GetPtr(VAR Arr : TArray; Index : WORD) : POINTER;
+PROCEDURE Put(VAR Arr : TArray; Index : WORD; VAR Element);
+PROCEDURE Swap(VAR Arr : TArray; Index1, Index2 : WORD);
+
+PROCEDURE Save(VAR Arr : TArray; VAR f : DYNARR_FILE);
+FUNCTION Load(VAR Arr : TArray; VAR f : DYNARR_FILE) : BOOLEAN;
+PROCEDURE Copy(VAR SourceArr, DestArr : TArray);
+PROCEDURE InsertAll(VAR SourceArr : TArray; SourceStartIndex, Count: WORD; VAR DestArr : TArray; DestStartIndex : WORD);
+
+IMPLEMENTATION
+
+CONST
+ALLOC_STEP = 16;
+MAGIC      = $4411;
+
+PROCEDURE Init(VAR Arr : TArray; ElementSize : WORD);
+BEGIN
+        FillChar(Arr, SizeOf(TArray), #0);
+        Arr.ElementSize := ElementSize;
+END;
+
+PROCEDURE Free(VAR Arr : TArray);
+BEGIN
+        IF Arr.Data <> nil THEN FreeMem(Arr.Data, Arr.Capacity * Arr.ElementSize);
+        Arr.Data := nil;
+        Arr.Size := 0;
+        Arr.Capacity := 0;
+END;
+
+PROCEDURE Reset(VAR Arr : TArray);
+BEGIN
+        Arr.Size := 0;
+END;
+
+PROCEDURE Clear(VAR Arr : TArray; StartIndex, Count: WORD);
+BEGIN
+        IF (StartIndex >= Arr.Size) OR (Count = 0) THEN EXIT;
+        IF (StartIndex + Count > Arr.Size) THEN Count := Arr.Size - StartIndex;
+        FillChar((PCHAR(Arr.Data) + StartIndex * Arr.ElementSize)^
+        , Count * Arr.ElementSize
+        , #0);
+END;
+
+PROCEDURE ChangeCapacity(VAR Arr : TArray; NewCapacity : WORD);
+VAR     NewData : PCHAR;
+BEGIN
+        IF NewCapacity = 0 THEN Reset(Arr)
+        ELSE BEGIN
+                GetMem(NewData, NewCapacity * Arr.ElementSize);
+                IF NewData <> NIL THEN BEGIN
+                        IF Arr.Data <> nil THEN BEGIN
+                                Move(PCHAR(Arr.Data)^, NewData^, Arr.Size * Arr.ElementSize);
+                                IF NewCapacity > Arr.Size THEN
+                                      FillChar(NewData[Arr.Size * Arr.ElementSize]
+                                      , (NewCapacity - Arr.Size) * Arr.ElementSize
+                                      , #0);
+                                FreeMem(Arr.Data, Arr.Capacity * Arr.ElementSize);
+                        END;
+                        Arr.Data := NewData;
+                        Arr.Capacity := NewCapacity;
+                END;
+        END;
+END;
+
+PROCEDURE Add(VAR Arr : TArray; VAR Element);
+BEGIN
+        IF Arr.Size = Arr.Capacity THEN ChangeCapacity(Arr, Arr.Capacity + ALLOC_STEP);
+        Move(Element
+        , (PCHAR(Arr.Data) + Arr.Size * Arr.ElementSize)^
+        , Arr.ElementSize);
+        INC(Arr.Size);
+END;
+
+PROCEDURE Delete(VAR Arr : TArray; StartIndex, Count: WORD);
+BEGIN
+        IF (StartIndex >= Arr.Size) OR (Count = 0) THEN EXIT;
+        IF (StartIndex + Count > Arr.Size) THEN Count := Arr.Size - StartIndex;
+        IF StartIndex + Count < Arr.Size THEN
+                Move((PCHAR(Arr.Data) + (StartIndex + Count) * Arr.ElementSize)^
+                , (PCHAR(Arr.Data) + StartIndex * Arr.ElementSize)^
+                , (Arr.Size - StartIndex - Count) * Arr.ElementSize);
+        DEC(Arr.Size, Count);
+        IF Arr.Size + ALLOC_STEP < Arr.Capacity - ALLOC_STEP * 2 THEN 
+                ChangeCapacity(Arr, Arr.Size + ALLOC_STEP);
+END;
+
+PROCEDURE Get(VAR Arr : TArray; Index : WORD; VAR Element);
+BEGIN
+        IF Index >= Arr.Size THEN EXIT;
+        Move((PCHAR(Arr.Data) + Index * Arr.ElementSize)^
+        , Element
+        , Arr.ElementSize);
+END;
+
+FUNCTION GetPtr(VAR Arr : TArray; Index : WORD) : POINTER;
+BEGIN
+        IF Index < Arr.Size THEN GetPtr := PCHAR(Arr.Data) + Index * Arr.ElementSize
+        ELSE GetPtr := NIL;
+END;
+
+PROCEDURE Put(VAR Arr : TArray; Index : WORD; VAR Element);
+BEGIN
+        IF Index >= Arr.Size THEN EXIT;
+        Move(Element
+        , (PCHAR(Arr.Data) + Index * Arr.ElementSize)^
+        , Arr.ElementSize);
+END;
+
+FUNCTION GetLength(VAR Arr : TArray) : WORD;
+BEGIN
+        GetLength := Arr.Size;
+END;
+
+PROCEDURE SetLength(VAR Arr : TArray; NewLength : WORD);
+BEGIN
+        IF Arr.Capacity < NewLength THEN ChangeCapacity(Arr, NewLength + ALLOC_STEP)
+        ELSE IF Arr.Capacity > NewLength + ALLOC_STEP THEN ChangeCapacity(Arr, NewLength);
+        Arr.Size := NewLength;
+END;
+
+PROCEDURE Save(VAR Arr : TArray; VAR f : DYNARR_FILE);
+VAR
+        w : WORD;
+BEGIN
+        w := MAGIC;
+        BlockWrite(f, w, SizeOf(WORD));
+        IF Arr.Data <> NIL THEN BEGIN
+                BlockWrite(f, Arr.Size, SizeOf(Arr.Size));
+                BlockWrite(f, Arr.ElementSize, SizeOf(Arr.ElementSize));
+                BlockWrite(f, PCHAR(Arr.Data)^, Arr.ElementSize * Arr.Size);
+        END ELSE BEGIN
+                w := 0;
+                BlockWrite(f, w, SizeOf(WORD));
+                BlockWrite(f, Arr.ElementSize, SizeOf(Arr.ElementSize));
+        END;
+        BlockWrite(f, w, SizeOf(WORD));
+END;
+
+FUNCTION Load(VAR Arr : TArray; VAR f : DYNARR_FILE) : BOOLEAN;
+VAR
+        w : WORD;
+        r : BOOLEAN;
+BEGIN
+        r := FALSE;
+        BlockRead(f, w, SizeOf(WORD));
+        IF w = MAGIC THEN BEGIN
+                BlockRead(f, Arr.Size, SizeOf(Arr.Size));
+                BlockRead(f, Arr.ElementSize, SizeOf(Arr.ElementSize));
+                GetMem(Arr.Data, Arr.Size * Arr.ElementSize);
+                Arr.Capacity := Arr.Size;
+                BlockRead(f, PCHAR(Arr.Data)^, Arr.Size * Arr.ElementSize);
+                r := TRUE;
+        END;
+        IF NOT r THEN Free(Arr);
+        Load := r;
+END;
+
+PROCEDURE Copy(VAR SourceArr, DestArr : TArray);
+BEGIN
+        DestArr.ElementSize := SourceArr.ElementSize;
+        SetLength(DestArr, SourceArr.Size);
+        Move(PCHAR(SourceArr.Data)^
+        , PCHAR(DestArr.Data)^
+        , SourceArr.Size * SourceArr.ElementSize);
+END;
+
+PROCEDURE InsertAll(VAR SourceArr : TArray; SourceStartIndex, Count: WORD; VAR DestArr : TArray; DestStartIndex : WORD);
+VAR     MoveCount: WORD;
+BEGIN
+        IF (SourceStartIndex + Count > SourceArr.Size) OR (Count = 0) THEN EXIT;
+        IF DestStartIndex > DestArr.Size THEN EXIT;
+        IF SourceArr.ElementSize <> DestArr.ElementSize THEN EXIT;
+  
+        MoveCount := DestArr.Size - DestStartIndex;
+        SetLength(DestArr, DestArr.Size + Count);
+  
+        IF MoveCount > 0 THEN
+                Move(PCHAR(DestArr.Data)[DestStartIndex * DestArr.ElementSize]
+                , PCHAR(DestArr.Data)[(DestStartIndex + Count)* DestArr.ElementSize]
+                , MoveCount * DestArr.ElementSize);
+  
+        Move(PCHAR(SourceArr.Data)[SourceStartIndex * SourceArr.ElementSize]
+        , PCHAR(DestArr.Data)[DestStartIndex * DestArr.ElementSize]
+        , Count * SourceArr.ElementSize);
+        INC(DestArr.Size, Count);
+END;
+
+PROCEDURE Insert(VAR Arr : TArray; Index: WORD; VAR Element);
+BEGIN
+        IF Index > Arr.Size THEN EXIT;
+        IF Arr.Size = Arr.Capacity THEN SetLength(Arr, Arr.Capacity + ALLOC_STEP);
+
+        IF Index < Arr.Size THEN
+                Move(PCHAR(Arr.Data)[Index * Arr.ElementSize]
+                , PCHAR(Arr.Data)[(Index + 1) * Arr.ElementSize]
+                , (Arr.Size - Index) * Arr.ElementSize);
+
+        Move(Element, PCHAR(Arr.Data)[Index * Arr.ElementSize], Arr.ElementSize);
+        INC(Arr.Size);
+END;
+
+PROCEDURE Swap(VAR Arr : TArray; Index1, Index2 : WORD);
+VAR     Allocated  : BOOLEAN;
+        TempBuffer : PCHAR;
+        a1, a2     : PCHAR;
+BEGIN
+        IF (Index1 >= Arr.Size) OR (Index2 >= Arr.Size) THEN EXIT;
+
+        Allocated := Arr.Capacity = Arr.Size;
+        IF Allocated THEN GetMem(TempBuffer, Arr.ElementSize)
+        ELSE TempBuffer := PCHAR(Arr.Data) + Arr.Size * Arr.ElementSize;
+
+        a1 := PCHAR(Arr.Data) + Index1 * Arr.ElementSize;
+        a2 := PCHAR(Arr.Data) + Index2 * Arr.ElementSize;
+
+        Move(a1^, TempBuffer^, Arr.ElementSize);
+        Move(a2^, a1^, Arr.ElementSize);
+        Move(TempBuffer^, a2^, Arr.ElementSize);
+
+        IF Allocated THEN FreeMem(TempBuffer, Arr.ElementSize) ELSE FillChar(TempBuffer^, Arr.ElementSize, #0);
+END;
+
+END.
+
---
+++ hashmap.pas
@@ -0,0 +1,203 @@
+{ MIT License
+
+Copyright (c) 2022 Viacheslav Komenda
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. }
+{$A+,I-,S-,R-,D+,L+,Q-,F-,G-,O-,B-}
+UNIT HashMap;
+
+INTERFACE
+
+CONST
+TABLE_SIZE = 16;
+MAX_KEY_LENGTH = 32;
+
+TYPE
+TMapKeyStr = STRING[MAX_KEY_LENGTH];
+TMapKey = RECORD
+    Key  : TMapKeyStr;
+    Hash : WORD;
+END;
+
+PHashEntry = ^THashEntry;
+THashEntry = RECORD
+    Key   : TMapKey;
+    Value : POINTER;
+    Next  : PHashEntry;
+END;
+
+PHashMap = ^THashMap;
+THashMap = RECORD
+    Table       : ARRAY[0..TABLE_SIZE-1] OF PHashEntry;
+    ElementSize : WORD;
+END;
+
+PROCEDURE Init(VAR Table: THashMap; ElementSize : WORD);
+PROCEDURE Clear(VAR Table: THashMap);
+FUNCTION ContainsKey(VAR Table: THashMap; Key: STRING): BOOLEAN;
+FUNCTION Get(VAR Table: THashMap; Key: STRING; VAR Value): BOOLEAN;
+FUNCTION GetPtr(VAR Table: THashMap; Key: STRING): POINTER;
+PROCEDURE Put(VAR Table: THashMap; Key: STRING; VAR Value);
+FUNCTION Remove(VAR Table: THashMap; Key: STRING): BOOLEAN;
+
+IMPLEMENTATION
+
+FUNCTION HashCode(VAR Key: TMapKeyStr): WORD;
+VAR
+  i: WORD;
+  Hash: WORD;
+BEGIN
+  Hash := 0;
+  FOR i := 1 TO Length(Key) DO INC(Hash, Hash * 48 + Ord(Key[i]));
+  HashCode := Hash;
+END;
+
+FUNCTION TableIndex(Hash : WORD): INTEGER;
+BEGIN
+        TableIndex := Hash MOD TABLE_SIZE;
+END;
+
+FUNCTION CreateHashEntry(VAR Table: THashMap; Next : PHashEntry; VAR Key: TMapKey; VAR value): PHashEntry;
+VAR
+  P: PHashEntry;
+BEGIN
+  GetMem(P, SizeOf(THashEntry));
+  P^.Key := Key;
+  P^.Next := Next;
+  GetMem(P^.Value, Table.ElementSize);
+  Move(value, P^.Value^, Table.ElementSize);
+  CreateHashEntry := P;
+END;
+
+PROCEDURE Init(VAR Table: THashMap; ElementSize : WORD);
+BEGIN
+  FillChar(Table, SizeOf(THashMap), 0);
+  Table.ElementSize := ElementSize;
+END;
+
+PROCEDURE CreateKeys(VAR s : STRING; VAR k : TMapKey);
+BEGIN
+        k.Key := s;
+        k.Hash := HashCode(k.Key);
+END;
+
+FUNCTION CompareKeys(VAR k1, k2 : TMapKey) : BOOLEAN;
+BEGIN
+        IF k1.Hash = k2.Hash THEN CompareKeys := k1.Key = k2.Key ELSE CompareKeys := FALSE;
+END;
+
+FUNCTION GetOrCreate(VAR Table: THashMap; Key: STRING; create : BOOLEAN) : PHashEntry;
+VAR
+  Entry: PHashEntry;
+  newKey: TMapKey;
+  tIndex : INTEGER;
+BEGIN
+  CreateKeys(Key, newKey);
+  tIndex := TableIndex(newKey.Hash);
+  Entry := Table.Table[tIndex];
+  
+  WHILE Entry <> NIL DO BEGIN
+    IF CompareKeys(Entry^.Key, newKey) THEN BEGIN
+      GetOrCreate := Entry;
+      EXIT;
+    END;
+    Entry := Entry^.Next;
+  END;
+  IF create THEN BEGIN
+          Entry := CreateHashEntry(Table, Table.Table[tIndex], newKey, newKey);
+        Table.Table[tIndex] := Entry;
+  END;
+  GetOrCreate := Entry;
+END;
+
+FUNCTION Get(VAR Table: THashMap; Key: STRING; VAR Value): BOOLEAN;
+VAR
+  Entry: PHashEntry;
+BEGIN
+  Entry := GetOrCreate(Table, Key, FALSE);
+  IF Entry <> NIL THEN Move(Entry^.Value^, Value, Table.ElementSize);
+  Get := Entry <> NIL;
+END;
+
+FUNCTION GetPtr(VAR Table: THashMap; Key: STRING): POINTER;
+VAR
+  Entry: PHashEntry;
+BEGIN
+  Entry := GetOrCreate(Table, Key, FALSE);
+  IF Entry <> NIL THEN GetPtr := Entry^.Value ELSE GetPtr := NIL;
+END;
+
+FUNCTION Remove(VAR Table: THashMap; Key: STRING): BOOLEAN;
+VAR
+  Entry, Prev: PHashEntry;
+  newKey: TMapKey;
+  tIndex : INTEGER;
+BEGIN
+  CreateKeys(Key, newKey);
+  tIndex := TableIndex(newKey.Hash);
+  Entry := Table.Table[tIndex];
+  Prev := NIL;
+ 
+  WHILE Entry <> NIL DO BEGIN
+    IF CompareKeys(Entry^.Key, newKey) THEN BEGIN
+      IF Prev = NIL THEN Table.Table[tIndex] := Entry^.Next
+      ELSE Prev^.Next := Entry^.Next;
+      FreeMem(Entry^.Value, Table.ElementSize);
+      FreeMem(Entry, SizeOf(THashEntry));
+      Remove := TRUE;
+      EXIT;
+    END;
+    Prev := Entry;
+    Entry := Entry^.Next;
+  END;
+  
+  Remove := FALSE;
+END;
+
+PROCEDURE Clear(VAR Table: THashMap);
+VAR
+  i: WORD;
+  Entry, Next: PHashEntry;
+BEGIN
+  FOR i := 0 TO TABLE_SIZE-1 DO BEGIN
+    Entry := Table.Table[i];
+    WHILE Entry <> NIL DO BEGIN
+      Next := Entry^.Next;
+      FreeMem(Entry^.Value, Table.ElementSize);
+      FreeMem(Entry, SizeOf(THashEntry));
+      Entry := Next;
+    END;
+  END;
+  FillChar(Table, SizeOf(THashMap), 0);
+END;
+
+PROCEDURE Put(VAR Table: THashMap; Key: STRING; VAR Value);
+VAR
+  Entry : PHashEntry;
+BEGIN
+  Entry := GetOrCreate(Table, Key, TRUE);
+  Move(Value, Entry^.Value^, Table.ElementSize);
+END;
+
+FUNCTION ContainsKey(VAR Table: THashMap; Key: STRING): BOOLEAN;
+BEGIN
+  ContainsKey := GetOrCreate(Table, Key, FALSE) <> NIL;
+END;
+
+END.
